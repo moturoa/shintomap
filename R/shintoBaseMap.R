780f23976dfb1772a55b25bac286eef554ff14f5
Remko Duursma <remko@shintolabs.nl>
2022-11-17 10:09:09 +0100

    shintobasemap args

--- R/shintoBaseMap.R
+++ R/shintoBaseMap.R
@@ -1,9 +1,18 @@
 #' Makes a leaflet map for use in shintoMapModule
 #' @export
-shintoBaseMap <- function(set_view = NULL, ...){
+shintoBaseMap <- function(set_view = NULL,
+                          fullscreen_option = FALSE,
+                          layers_button_position = "topright",
+                          ...){
 
   map <- leaflet::leaflet() %>%
-    addShintoMapLayers(...)
+    addShintoMapLayers(..., position = layers_button_position)
+
+
+  if(fullscreen_option){
+    map <- map %>%
+      addFullscreenControl(pseudoFullscreen=TRUE)
+  }
 
   # Set the initial location
   # --> if set_view used, map does not rescale when border() changes
