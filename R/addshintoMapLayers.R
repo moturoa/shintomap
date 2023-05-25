59dd40527f1c252afb59d1c2952ed59ef6dd3f63
Remko Duursma <remko@shintolabs.nl>
2023-05-25 10:57:48 +0200

    map layers improvements

--- R/addshintoMapLayers.R
+++ R/addshintoMapLayers.R
@@ -1,10 +1,27 @@
-#' Add map layers
+#' Add map tiles to a leaflet map, shinto style
+#' @description Standardized map tiles for Shinto applications. Standaard NL, ESRI gray, OpenStreetMap,
+#' Kadaster map (overlayed over standaard NL with transparency), PDOK arial photos (RGB, IR)
+#' @param map A Leaflet map object
+#' @param default Default selected layer
+#' @param openstreetmap_extend_zoom If TRUE, extends zoom in OpenStreetMap layer beyond native zoom,
+#' so we can zoom in further (with lower resolution). Useful for drawing on map.
+#' @param kadaster_opacity Opacity of the kadaster map overlayed over the Standaard NL map. If you don't want to
+#' see the standaard NL map, set opacity to 1.0.
 #' @export
 #' @importFrom leaflet addProviderTiles addTiles addWMSTiles WMSTileOptions addLayersControl
 addShintoMapLayers <- function(map, default = "Esri.WorldGrayCanvas",
                                position = "topright",
-                               openstreetmap_extend_zoom = FALSE
+                               openstreetmap_extend_zoom = FALSE,
+                               kadaster_opacity = 0.5
                                ){
+
+
+  urls <- list(
+    pdok_standaard_nl = "https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/standaard/EPSG:3857/{z}/{x}/{y}.png",
+    pdok_luchtfoto_rgb = "https://service.pdok.nl/hwh/luchtfotorgb/wms/v1_0",
+    pdok_luchtfoto_ir = "https://service.pdok.nl/hwh/luchtfotocir/wms/v1_0",
+    geodata_kadaster = "https://geodata.nationaalgeoregister.nl/kadastralekaart/wms/v4_0"
+  )
 
   tiles <- c(
     "Kaart - grijs (ESRI)" = "Esri.WorldGrayCanvas",
@@ -20,10 +37,11 @@ addShintoMapLayers <- function(map, default = "Esri.WorldGrayCanvas",
   for(i in seq_along(tiles)){
 
     # Max zoom extenden (voor intekenen gebied in WBM zodat je de kaart verder dan de native max zoom kan inzoomen)
+    # set maxZoom hoger om nog verder in te kunnen zoomen (met steeds lagere resolutie)
     if(names(tiles[i]) == "OpenStreetMap" & openstreetmap_extend_zoom){
       map <- map %>%
         leaflet::addProviderTiles(unname(tiles[i]), group = names(tiles[i]),
-                                  options = providerTileOptions(maxNativeZoom = 18, maxZoom = 22))
+                                  options = providerTileOptions(maxNativeZoom = 18, maxZoom = 22)) #
     } else {
       map <- map %>%
         leaflet::addProviderTiles(unname(tiles[i]), group = names(tiles[i]))
@@ -34,28 +52,31 @@ addShintoMapLayers <- function(map, default = "Esri.WorldGrayCanvas",
 
 
   map <- map %>%
-    leaflet::addTiles(urlTemplate = "https://service.pdok.nl/brt/achtergrondkaart/wmts/v2_0/standaard/EPSG:3857/{z}/{x}/{y}.png",
+    leaflet::addTiles(urlTemplate = urls$pdok_standaard_nl,
              group = "Standaard NL (PDOK BRT)") %>%
 
     leaflet::addWMSTiles(
-      "https://service.pdok.nl/hwh/luchtfotorgb/wms/v1_0",
+      urls$pdok_luchtfoto_rgb,
       layers = "Actueel_orthoHR",
       group = "Luchtfoto Actueel HighRes (PDOK)",
       options = WMSTileOptions(maxNativeZoom = 22, maxZoom = 24)
     ) %>%
 
     leaflet::addWMSTiles(
-      "https://service.pdok.nl/hwh/luchtfotocir/wms/v1_0",
+      urls$pdok_luchtfoto_ir,
       layers = "Actueel_ortho25IR",
       group = "Luchtfoto Actueel IR (PDOK)",
       options = WMSTileOptions(maxNativeZoom = 22, maxZoom = 24)
     ) %>%
 
+    # Kadaster is semi-transparent over standaard-nl
+    leaflet::addTiles(urlTemplate = urls$pdok_standaard_nl,
+                      group = "Kadastrale kaart") %>%
     leaflet::addWMSTiles(
-      "https://geodata.nationaalgeoregister.nl/kadastralekaart/wms/v4_0",
+      urls$geodata_kadaster,
       layers = "kadastralekaart",
       group = "Kadastrale kaart",
-      options = WMSTileOptions(maxNativeZoom = 18, maxZoom = 22))
+      options = WMSTileOptions(maxNativeZoom = 18, maxZoom = 22, opacity =  kadaster_opacity))
 
   map <- map %>%
     leaflet::addLayersControl(baseGroups = c(names(tiles),
